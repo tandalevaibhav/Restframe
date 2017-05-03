c092d347ba8d9728dbbc28b2fb8cb06e8eec2557
tandalevaibhav <vaibhav.t@amazatic.com>
2017-05-03 11:34:40 +0530

    used generic views of rest-framework

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,13 +1,22 @@
-from django.conf.urls import url
-from snippets import views
-from rest_framework.urlpatterns import format_suffix_patterns
-# urlpatterns = [
-#     url(r'^snippets/$', views.snippet_list),
-#     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
-# ]
+# from django.conf.urls import url
+# from snippets import views
+# from rest_framework.urlpatterns import format_suffix_patterns
+# # urlpatterns = [
+# #     url(r'^snippets/$', views.snippet_list),
+# #     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+# # ]
 
-urlpatterns=[
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
-    url(r'^snippets/', views.snippet_list),
+# urlpatterns=[
+#     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+#     url(r'^snippets/', views.snippet_list),
+# ]
+# urlpatterns = format_suffix_patterns(urlpatterns)
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+
+urlpatterns = [
+             url(r'^snippets/$', views.SnippetList.as_view()),
+             url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
